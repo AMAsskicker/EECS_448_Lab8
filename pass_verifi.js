c0376ff31670d34acdf6db957e50a484300bd6f5
Adam Abernathy <amabernathy@ku.edu>
2021-11-10 23:27:02 -0600

    added length requirement

--- pass_verifi.js
+++ pass_verifi.js
@@ -12,18 +12,27 @@
 document.addEventListener("DOMContentLoaded", () => {
 	//get input values and math operation to use
 	let input1 = document.querySelector("#input1");
+	// let input1 = document.getElementById('input1').value;
 	let input2 = document.querySelector("#input2");
+	// let input2 = document.getElementById('input2').value;
 
 	document.querySelector("#computeButton").addEventListener("click", (e) => {
 		//for troubleshooting
 		console.log('click');
 		console.log('input1', input1.value);
 		console.log('input2', input2.value);
+		let test1 = input1.value
+		let test2 = input2.value
 
-    if (input1.value == input2.value) {
-      result = "PASSWORDS MATCHED";
+    if ((test1.length < 8) || (test2.length < 8)){
+      result  = "PASSWORD MUST BE 8 CHARACTERS LONG"
     } else {
-      result = "PASSWORDS UNMATCHED";
+      // if (input1.value == input2.value) {
+			if (test1 == test2) {
+        result = "PASSWORDS MATCHED";
+      } else {
+        result = "PASSWORDS UNMATCHED";
+      }
     }
     document.querySelector("#result").innerText = result;
 	});
