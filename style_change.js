7d125a118ded724d79af7ec86462777cd0d01103
Adam Abernathy <amabernathy@ku.edu>
2021-11-14 19:08:03 -0600

    updated style to handle width

--- style_change.js
+++ style_change.js
@@ -13,30 +13,33 @@ document.addEventListener("DOMContentLoaded", () => {
 	//get input values and math operation to use
 	let border_color = document.querySelector("#border_input");
 	let back_color = document.querySelector("#background");
+	let bord_width = document.querySelector("#b_width")
 	var back_2_change = document.getElementById('paragraph')
+
 
 	document.querySelector("#apply_but").addEventListener("click", (e) => {
 		//for troubleshooting
 		console.log('click');
 		console.log('border_color', border_color.value);
 		console.log('back_color', back_color.value);
-		var new_back_color, new_border_color
+		console.log('b_width', b_width.value)
+		var new_back_color, new_border_color, new_width
 
 		switch (border_color.value) {
 			case "red":
-				new_border_color = "3px solid red"
+				new_border_color = "solid red"
 				break
 			case "green":
-				new_border_color = "3px solid green"
+				new_border_color = "solid green"
 				break
 			case "blue":
-				new_border_color = "3px solid blue"
+				new_border_color = "solid blue"
 				break
 			case "width":
-				new_border_color = "3px solid yellow"
+				new_border_color = "solid yellow"
 				break;
 			default:
-				new_border_color = "3px solid white"
+				new_border_color = "solid white"
 		}
 		switch (back_color.value) {
 			case "red":
@@ -51,8 +54,42 @@ document.addEventListener("DOMContentLoaded", () => {
 			default:
 				new_back_color = "white"
 		}
+		switch (b_width.value) {
+			case "0":
+				new_width = "0px "
+				break;
+			case "1":
+				new_width = "1px "
+				break
+			case "2":
+				new_width = "2px "
+				break
+			case "3":
+				new_width = "3px "
+				break;
+			case "4":
+				new_width = "4px "
+				break;
+			case "5":
+				new_width = "5px "
+				break;
+			case "6":
+				new_width = "6px "
+				break;
+			case "7":
+				new_width = "7px "
+				break;
+			case "8":
+				new_width = "8px "
+				break;
+			case "9":
+				new_width = "9px "
+				break;
+			default:
+				new_width = "0px "
+		}
 
     back_2_change.style.background = new_back_color
-		back_2_change.style.border = new_border_color
+		back_2_change.style.border = new_width + new_border_color
 	});
 });
